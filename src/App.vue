cfca215984e333018770e2a9deb638698b7a67de
Tatsu Sei <doubledragontaz@gmail.com>
2021-08-11 23:43:44 +0900

    get data at app component created

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,21 @@
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 
+import { mapActions } from "vuex";
+
 export default {
   name: "App",
   components: {
     Header,
     Footer,
+  },
+  methods: {
+    ...mapActions({
+      getEventData: "getEventData",
+    }),
+  },
+  created() {
+    this.getEventData();
   },
 };
 </script>
