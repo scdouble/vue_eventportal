a4cdac32d2a7a5ca26d8d103c03dc4f5c00e41d9
Tatsu Sei <doubledragontaz@gmail.com>
2021-08-11 15:24:00 +0900

    modify homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,18 @@
 <template>
-  <h1>トップページです </h1>
+  <h1>近日開催のイベント</h1>
+  <h2>注目キーワード</h2>
+  <p>#SDGs #DX　#テレワーク #参加無料</p>
+  <h2>新着イベント</h2>
+  <p>イベント。。。。。</p>
+  <h2>PICK UP</h2>
+  <p>イベント。。。。。</p>
+  <h2>まもなく締め切り</h2>
+  <p>イベント。。。。。</p>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
