852dbb58dabebe8aa9e03e317ba1db90b42b054a
Tatsu Sei <doubledragontaz@gmail.com>
2021-08-11 15:24:30 +0900

    add sample sentence to about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,5 +1,14 @@
 <template>
   <h1>About This Project</h1>
+  <article>
+    Et dolore laudantium explicabo nisi minus. Nostrum distinctio eos ullam
+    consequatur sed laborum eaque non. Quod non fugit sed. Distinctio quaerat
+    hic iusto quia. Velit doloremque officiis libero beatae nihil qui dolorum
+    laboriosam consequatur. Quia eos quis excepturi ullam quia laboriosam sit.
+    Repellendus ducimus quia eum. Debitis enim ea. Vel et ratione eius.
+    Doloremque doloribus consectetur. Officia non ullam iure aut similique
+    adipisci. Ullam commodi ut. Ex est saepe quasi aut impedit.
+  </article>
 </template>
 
 <script>
